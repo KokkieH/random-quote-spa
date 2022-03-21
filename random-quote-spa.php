d73c725dc722f79fd58c0dc06366e11febf1f890
KokkieH <KokkieH@users.noreply.github.com>
2022-03-21 10:07:37 +0200

    Initial commit

---
+++ random-quote-spa.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Random Quote Spa
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     random-quote-spa
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Random_Quote_Spa
+ */
+
+// Your code starts here.
