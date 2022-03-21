323b89156360c784967dd19b2f0e014b61d10714
KokkieH <KokkieH@users.noreply.github.com>
2022-03-21 13:56:28 +0200

    Add app media files Add markup using JSX Add CSS

--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import quill from './quill.png';
+import quill2 from './quill2.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="content">
+        <h1>Thus wrote the Bard...</h1>
+        <div className="centered">
+          <p id="printQuote">This is a quote</p>
+          <p id="author">- William Shakespeare</p>
+          <img id="button" src={quill} alt="new quote button" 
+          onMouseOver={e => (e.currentTarget.src=quill2)}
+          onMouseOut={e => (e.currentTarget.src=quill)}
+          onclick="generateQuote();" />
+        </div>
+      </div>
+    <footer className="footer">
+      Powered by the STANDS4 Web Services <a href="http://www.quotes.net/quotes_api.php">Quotes API</a>
+    </footer>
+  </div>
   );
 }
 
