088fadf2cabc90b8fb09a43b46eb8291b068fd50
KokkieH <KokkieH@users.noreply.github.com>
2022-03-21 10:22:44 +0200

    Complete plugin header info

--- random-quote-spa.php
+++ random-quote-spa.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:     Random Quote Spa
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
- * Text Domain:     random-quote-spa
+ * Plugin Name:     Random Quote Single Page Application
+ * Plugin URI:      https://github.com/KokkieH/random-quote-spa
+ * Description:     Random quote generator
+ * Author:          KokkieH
+ * Author URI:      https://kokkieh.blog/
+ * Text Domain:     kokkieh-random-quote-spa
  * Domain Path:     /languages
  * Version:         0.1.0
  *
